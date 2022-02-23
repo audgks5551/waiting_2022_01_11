76f8a94133e62225d333f3e063e066504b2f163d
audgks5551 <audgks5551@daum.net>
2022-02-23 13:49:28 +0900

    base -> config 수정

--- git_pull_and_rerun.sh
+++ git_pull_and_rerun.sh
@@ -26,13 +26,13 @@ docker exec python__2 bash -ce "cd /data/site_projects/python__2/src/ ; git pull
 docker exec python__2 bash -ce "cd /data/site_projects/python__2/src/ ; pip install -r requirements/prod.txt"
 
 # 마이그레이트
-docker exec python__2 bash -ce "cd /data/site_projects/python__2/src/ ; python manage.py migrate --settings=base.settings.prod"
+docker exec python__2 bash -ce "cd /data/site_projects/python__2/src/ ; python manage.py migrate --settings=config.settings.prod"
 
 # 장고를 운영모드로 실행
-docker exec python__2 bash -ce "cd /data/site_projects/python__2/src ; nohup gunicorn --bind=0.0.0.0:8000 base.wsgi &"
+docker exec python__2 bash -ce "cd /data/site_projects/python__2/src ; nohup gunicorn --bind=0.0.0.0:8000 config.wsgi &"
 
 # static collect 다시 수행
-docker exec python__2 bash -ce "cd /data/site_projects/python__2/src ; echo yes | python manage.py collectstatic --settings=base.settings.prod"
+docker exec python__2 bash -ce "cd /data/site_projects/python__2/src ; echo yes | python manage.py collectstatic --settings=config.settings.prod"
 
 exit 0
 
